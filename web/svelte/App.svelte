a374ade489ea464917fc18155885d64345d7a214
Fire-The-Fox <64907994+Fire-The-Fox@users.noreply.github.com>
2022-09-04 10:57:36 -0700

    Add files via upload

--- web/svelte/App.svelte
+++ web/svelte/App.svelte
@@ -1,36 +1,16 @@
 <script>
-    // https://svelte.dev/examples/spring
-	import { spring } from 'svelte/motion';
+	import { time } from './store.js';
 
-	let coords = spring({ x: 50, y: 50 }, {
-		stiffness: 0.1,
-		damping: 0.25
+	const formatter = new Intl.DateTimeFormat('en', {
+		hour12: true,
+		hour: 'numeric',
+		minute: '2-digit',
+		second: '2-digit'
 	});
 
-	let size = spring(10);
+    $: {
+        document.title = formatter.format($time);
+    }
 </script>
 
-<div style="position: absolute; right: 1em;">
-	<label>
-		<h3>stiffness ({coords.stiffness})</h3>
-		<input bind:value={coords.stiffness} type="range" min="0" max="1" step="0.01">
-	</label>
-
-	<label>
-		<h3>damping ({coords.damping})</h3>
-		<input bind:value={coords.damping} type="range" min="0" max="1" step="0.01">
-	</label>
-</div>
-
-<svg
-	on:mousemove="{e => coords.set({ x: e.clientX, y: e.clientY })}"
-	on:mousedown="{() => size.set(30)}"
-	on:mouseup="{() => size.set(10)}"
->
-	<circle cx={$coords.x} cy={$coords.y} r={$size}/>
-</svg>
-
-<style>
-	svg { width: 100%; height: 100vh; margin: -8px }
-	circle { fill: #ff3e00 }
-</style>
+<h1>The time is {formatter.format($time)}</h1>
