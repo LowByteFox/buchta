796246b9e03852db76ae924d0180b2521d414e98
FireTheFox <gajdos.jan77@gmail.com>
2022-12-13 20:04:04 +0100

    added binary once again

---
+++ bin/buchta.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env bun
+process.argv = process.argv.slice(2);
+
+if (process.argv.length == 0) {
+    console.log('Usage: buchta [command]');
+    console.log('Commands:');
+    console.log('  init\t\tCreates basic project structure');
+    console.log('  serve\t\tStarts a local server');
+    process.exit(0);
+}
+
+const command = process.argv[0];
